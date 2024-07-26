eac7615f50b1cffe3ad1374ac4c79d6d55b34632
Katarine L. S. Alves <66075299+katar1ne@users.noreply.github.com>
2024-07-25 21:48:48 -0300

    Update dog_breed_service.dart

--- lib/services/dog_breed_service.dart
+++ lib/services/dog_breed_service.dart
@@ -7,14 +7,12 @@ import '../models/dog_breed.dart';
 class DogBreedService {
   Future<List<DogBreed>> fetchDogBreeds(
       {required int startIndex, required int limit}) async {
-    // Simulando a busca de um arquivo JSON local
     final String response =
         await rootBundle.loadString('assets/dog_breeds.json');
     final List<dynamic> data = json.decode(response);
     final List<DogBreed> allBreeds =
         data.map((json) => DogBreed.fromJson(json)).toList();
 
-    // Retornando uma sublista com base no índice de início e no limite
     return allBreeds.skip(startIndex).take(limit).toList();
   }
 }
